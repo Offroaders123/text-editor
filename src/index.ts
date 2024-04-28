d188470804a6e82c1175e62c0a7489ccfb0e2b51
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-28 03:36:25 -0700

    Index Entry File
    Moving things over to a more module-friendly setup, since I want to build it with a framework eventually as well.

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "./analytics.js";
+
+import "./idb-keyval-iife.js";
+
+import "./rum.js";
+import "./app.js";
+
+import "./menus.js";
+
+import "./text-area.js";
+import "./fs-helpers.js";
+import "./menu-file.js";
+import "./menu-edit.js";
+import "./menu-view.js";
+import "./menu-recent.js";
+import "./ui.js";
+import "./keyboard-events.js";
+import "./load-sw.js";
+import "./app-install.js";
+import "./fallback.js";
