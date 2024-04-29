0ca3e91f0151909db947881ba2c1b76af6cfaf25
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-29 00:16:11 -0700

    Indentation Preservation
    Migrating from HTML to JSX has some challenges, mostly in places where HTML's formatting is used to dictate where whitespace should be in the page. JSX doesn't persist that losslessly quite right, so you have to escape it with a string literal and brackets. I ran into this same issue with Dovetail too.

--- src/AppComponent.tsx
+++ src/AppComponent.tsx
@@ -106,25 +106,25 @@ export default function AppComponent() {
             <span id="lblTabMovesFocus" class="hidden footer-label">Tab Moves Focus</span>
           </span>
         </summary>
-        <div id="not-supported">
+        <div id="not-supported">{`
           The
-          <a href="https://wicg.github.io/file-system-access/" target="_blank">File System Access API</a>
-          is <b>not</b> supported in this browser yet, and Text Editor is running
+          `}<a href="https://wicg.github.io/file-system-access/" target="_blank">File System Access API</a>{`
+          is `}<b>not</b>{` supported in this browser yet, and Text Editor is running
           in legacy mode. In legacy mode, the file modified indicators are not shown.
-        </div>
-        <div>
+        `}</div>
+        <div>{`
           Text Editor is a simple text editor similar to notepad that makes
           it easy to edit text files on your local file system. It shows how
           to easily use the new HTML5 File System Access APIs. To learn more
           about the File System Access APIs and how this demo was built, check out
-          <a href="https://web.dev/file-system-access/" target="_blank">
-          The file system access APIs</a> article on Web Fundamentals, or see the
-          <a href="https://github.com/GoogleChromeLabs/text-editor/" target="_blank">
-          source code on GitHub</a>.
-          Written by <a href="https://twitter.com/petele" target="_blank">Pete LePage</a>.
-          <small id="lastUpdated">
+          `}<a href="https://web.dev/file-system-access/" target="_blank">{`
+          The file system access APIs`}</a>{` article on Web Fundamentals, or see the
+          `}<a href="https://github.com/GoogleChromeLabs/text-editor/" target="_blank">{`
+          source code on GitHub`}</a>{`.
+          Written by `}<a href="https://twitter.com/petele" target="_blank">Pete LePage</a>{`.
+          `}<small id="lastUpdated">{`
             Last Updated: [[BUILD_DATE]] (v[[VERSION]])
-          </small>
+          `}</small>
         </div>
       </details>
     </>
